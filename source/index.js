ee817f16b403d7d8ecb9858d789df8105048d503
ThatBean <Goomailbean@gmail.com>
2016-12-22 16:49:03 +0800

    fix gitignore

---
+++ source/index.js
@@ -0,0 +1,22 @@
+import { Scheme, MapScheme, ListScheme } from './scheme'
+import { MapOf, ListOf, mapActionMap, listActionMap } from './creator'
+
+export {
+  Scheme,
+  MapScheme,
+  ListScheme,
+  MapOf,
+  ListOf,
+  mapActionMap,
+  listActionMap
+}
+
+export default {
+  Scheme,
+  MapScheme,
+  ListScheme,
+  MapOf,
+  ListOf,
+  mapActionMap,
+  listActionMap
+}
